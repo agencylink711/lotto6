7a3f6eb4f6658feec094a7adadf78cb597311a03
petesusmac <agencylink711@gmail.com>
2025-07-07 16:40:36 +0200

    ALEMBIC reflex db init INITIATED Enhance project configuration: add Alembic setup, update database URL handling, and include dotenv support.

--- rxconfig.py
+++ rxconfig.py
@@ -1,12 +1,16 @@
-import reflex as rx
 import os
+import reflex as rx
+from dotenv import load_dotenv
 
-# Get the database URL from environment variable
-db_url = os.getenv("DATABASE_URL", "postgresql://petesusmac@localhost:5432/lotto6aus49")
+load_dotenv()  # Load environment variables from .env file
+
+# Get the database URL from environment variable - never expose credentials in code
+DATABASE_URL = os.environ.get("DATABASE_URL")
+print(DATABASE_URL)  # Debugging line to ensure the URL is loaded correctly
 
 config = rx.Config(
-    app_name="lotto6",
-    db_url=db_url,  # Configure the database connection
+    app_name="lotto6",  # Must match your folder name
+    db_url=DATABASE_URL,  # Configure the database connection
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
